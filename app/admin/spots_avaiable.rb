1da5dff2c5d7c36d6c6b3451403be350f443af2b
Raghu Betina <raghu@firstdraft.com>
2016-12-13 06:38:14 +0000

    Whitelist columns in SpotsAvaiable for ActiveAdmin

--- app/admin/spots_avaiable.rb
+++ app/admin/spots_avaiable.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SpotsAvaiable do
 
+ permit_params :streets, :time, :thanks, :neighborhood_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
