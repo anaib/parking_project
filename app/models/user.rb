cc512d819d005559ff3fcbf1995e71fe6487c68d
Raghu Betina <raghu@firstdraft.com>
2016-12-13 06:37:47 +0000

    Direct association between users and parking_spots

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :parking_spots,
+             :foreign_key => "offer_user_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
