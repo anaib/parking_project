1665b8d734e4ae14c55882d9453033c98d619b82
Raghu Betina <raghu@firstdraft.com>
2016-12-13 06:38:08 +0000

    Whitelist columns in Car for ActiveAdmin

--- app/admin/car.rb
+++ app/admin/car.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Car do
 
+ permit_params :type_of_car, :car_manf, :car_model, :car_registration, :car_color, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
