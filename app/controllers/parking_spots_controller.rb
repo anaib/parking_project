fc211f237cdc3f104d0969524dab290960bad689
Raghu Betina <raghu@firstdraft.com>
2016-12-13 06:38:14 +0000

    Only parking_spot user can modify parking_spot

--- app/controllers/parking_spots_controller.rb
+++ app/controllers/parking_spots_controller.rb
@@ -1,4 +1,14 @@
 class ParkingSpotsController < ApplicationController
+  before_action :current_user_must_be_parking_spot_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_parking_spot_user
+    parking_spot = ParkingSpot.find(params[:id])
+
+    unless current_user == parking_spot.offer_user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @parking_spots = ParkingSpot.all
 
