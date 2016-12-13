a7387383106f36fbe22daa07f7d4bd66eeebdbae
Raghu Betina <raghu@firstdraft.com>
2016-12-13 06:38:05 +0000

    Whitelist columns in ParkingSpot for ActiveAdmin

--- app/admin/parking_spot.rb
+++ app/admin/parking_spot.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ParkingSpot do
 
+ permit_params :address, :city, :zipcode, :time, :car_type, :offer_user_id, :type_of_parking, :max_time, :price, :description, :accept_user_id, :neighborhood
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
