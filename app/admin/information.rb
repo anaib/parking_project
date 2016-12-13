c05f81725b36f1bca334f62021175438aa7f5683
Raghu Betina <raghu@firstdraft.com>
2016-12-13 06:38:11 +0000

    Whitelist columns in Information for ActiveAdmin

--- app/admin/information.rb
+++ app/admin/information.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Information do
 
+ permit_params :city, :neighborhood, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
