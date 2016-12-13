9f52e74abfe6bac803f5745ed04e7d01100b5538
Raghu Betina <raghu@firstdraft.com>
2016-12-13 06:37:47 +0000

    Validate presence of users.last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :last_name, :presence => true
+
   validates :phone_number, :uniqueness => true
 
   validates :phone_number, :presence => true
