0c0905c33278da6c7432955317d8c97d4c6b1597
Raghu Betina <raghu@firstdraft.com>
2016-12-15 05:13:35 +0000

    10th draft

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User do
 
- permit_params :first_name, :last_name, :city, :phone_number, :points, :email, :password, :car_registration
+ permit_params :first_name, :last_name, :city, :phone_number, :points, :email, :password, :car_registration, :username
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
